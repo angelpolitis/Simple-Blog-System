858877ab81525373cd6e2036cfdea28eb0fb6310
Angel Politis <info@angelpolitis.com>
2025-11-08 00:36:33 +0200

    Created an AuthServiceProvider as well as the models, migrations and policy for restricting who can create/edit/delete posts.

---
+++ src/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\Post;
+use App\Policies\PostPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     */
+    protected $policies = [
+        // Register your policies here
+        Post::class => PostPolicy::class,
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
