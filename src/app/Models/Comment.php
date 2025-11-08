a5d578d9ba308199862ae30e85d820b5218f60d2
Angel Politis <info@angelpolitis.com>
2025-11-08 06:16:28 +0200

    Modified the Post and Comment models to allow for mass-updating of safe fields.

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
-{
+class Comment extends Model {
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'comment',
+    ];
+    
     public function post() {
         return $this->belongsTo(Post::class);
     }
