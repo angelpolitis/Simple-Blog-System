02bdb39f9e3208700093ebf8a820b4883ee76db4
Angel Politis <info@angelpolitis.com>
2025-11-08 06:07:45 +0200

    Created the personalised request classes used for creating and updating a post and creating a comment.

---
+++ src/app/Http/Requests/CommentRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentRequest extends FormRequest {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize () : bool {
+        # We give authorisation here and the policy will handle the matter of the ownership of the post.
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules () : array {
+        return [
+            'comment' => 'required|string',
+        ];
+    }
+}
---
+++ src/app/Http/Requests/PostRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize () : bool {
+        # We give authorisation here and the policy will handle the matter of the ownership of the post.
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules () : array {
+        return [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ];
+    }
+}
